a33eb17a7ef9f1122d297d4a1ddad72668236f01
jaewon Jang <dodo4421@naver.com>
2023-12-03 19:41:27 +0900

    add

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header"
+import Mainpage from "@/components/Mainpage"
+import Footer from "@/components/Footer"
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Mainpage />
+      <Footer />
+    </div>
+  )
+}
